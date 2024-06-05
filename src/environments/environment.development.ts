aebf8131e5ed90a1c582fd97390c521c551531aa
Riguja89 <jaimegutierrios@gmail.com>
2024-06-05 10:58:52 -0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiUrl: 'https://661eccfa16358961cd92dba5.mockapi.io',
+  apiUrlAuth:'http://localhost:8080/api/v1/auth',
+  apiUrlReading:'http://localhost:8080/api/v1/reading',
+  apiUrlQuotes:'http://localhost:8080'
+};
