6d12348a9ed41b1711ad38218c4e8cb4e1c4372b
ronaldo-manrique <ronaldomanrique123@gmail.com>
2024-06-05 13:54:48 -0500

    add header

--- src/app/container/header-container/header-container.component.ts
+++ src/app/container/header-container/header-container.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { HeaderBlockComponent } from '../../ui/blocks/header-block/header-block.component';
+import { HeaderContainerFacade } from './header.facade';
 
 @Component({
   selector: 'app-header-container',
   standalone: true,
-  imports: [],
+  imports: [HeaderBlockComponent],
   templateUrl: './header-container.component.html',
-  styleUrl: './header-container.component.css'
+ 
 })
 export class HeaderContainerComponent {
+
+  constructor(
+    private readonly facade:HeaderContainerFacade
+  ) { }
+
+  remove(){    
+    this.facade.remove('token');    
+   }
+
+
+
+
 
 }
