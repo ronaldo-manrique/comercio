90c5f5f759d300e5f5ca3647b97f87f1afea6e57
ronaldo-manrique <ronaldomanrique123@gmail.com>
2024-06-07 14:39:44 -0500

    modify styles cart

--- src/app/core/resources/url.resources.ts
+++ src/app/core/resources/url.resources.ts
@@ -7,9 +7,9 @@ export const URL_RESOURCES = {
     copies:"https://6660c43a5425580055b528e7.mockapi.io/api/v1/copies",
 
     loanCopies:"https://provider-comerce-1.onrender.com/comerce/loan",
-    //token:"https://run.mocky.io/v3/3f7e6305-7cdb-4130-a9fd-1f286afacea6"
+    token:"https://run.mocky.io/v3/3f7e6305-7cdb-4130-a9fd-1f286afacea6"
 
-    token:"https://pinguinera-module-auth.onrender.com/api/v1/token/refresh"
+    //token:"https://pinguinera-module-auth.onrender.com/api/v1/token/refresh"
 
 }
 
