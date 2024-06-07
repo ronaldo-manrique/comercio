49a419e670df7bbd0845c7cc7b4a53e1adccab68
ronaldo-manrique <ronaldomanrique123@gmail.com>
2024-06-07 14:15:24 -0500

    add styles cart

---
+++ src/app/ui/elements/modal-success-element/modal-success-element.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-success-element',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-success-element.component.html',
+  styleUrl: './modal-success-element.component.css'
+})
+export class ModalSuccessElementComponent {
+
+}
