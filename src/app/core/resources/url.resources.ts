b92deca862a6586d77ac8fae4408aa44cf2f21de
Riguja89 <jaimegutierrios@gmail.com>
2024-06-07 13:54:13 -0500

    add style copies

--- src/app/core/resources/url.resources.ts
+++ src/app/core/resources/url.resources.ts
@@ -2,15 +2,15 @@ import { environment } from "../../../environments/environment.development";
 
 export const URL_RESOURCES = {
 
-    copies:"https://provider-comerce-1.onrender.com/literatures",
-    
-    //copies:"https://6660c43a5425580055b528e7.mockapi.io/api/v1/copies",
+    //copies:"https://provider-comerce-1.onrender.com/literatures",
+
+    copies:"https://6660c43a5425580055b528e7.mockapi.io/api/v1/copies",
 
     loanCopies:"https://provider-comerce-1.onrender.com/comerce/loan",
     //token:"https://run.mocky.io/v3/3f7e6305-7cdb-4130-a9fd-1f286afacea6"
 
     token:"https://pinguinera-module-auth.onrender.com/api/v1/token/refresh"
-    
+
 }
 
 
